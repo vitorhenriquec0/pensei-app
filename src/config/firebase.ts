8fb909417acb2854f730e4f6f61314ac490d7173
vitorhenriquec0 <vitorhenriquecardoso59@gmail.com>
2026-06-15 19:20:06 -0300

    feat: integrar Firebase Firestore e guardar novos cadernos na nuvem

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { initializeFirestore, memoryLocalCache } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArS7-Mh6DwYG1t6m-LU7THcCIHgK3og1E",
-  authDomain: "pensei-app.firebaseapp.com",
-  projectId: "pensei-app",
-  storageBucket: "pensei-app.firebasestorage.app",
-  messagingSenderId: "402181231408",
-  appId: "1:402181231408:web:49bb9952d10447d4e12ef5",
-  measurementId: "G-PJJHQNQSK8"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEANSUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
